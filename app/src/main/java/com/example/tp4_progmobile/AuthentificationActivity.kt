53911e38dcec601c7c3e99e1b3cb9e1734412bcb
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-11-17 09:33:51 -0500

    +Authentification Acivity

---
+++ app/src/main/java/com/example/tp4_progmobile/AuthentificationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tp4_progmobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthentificationActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

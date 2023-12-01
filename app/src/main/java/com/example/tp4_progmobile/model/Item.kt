42798e0eb95a8bc235ad86ed458a1d6e02315dcc
Guillaume Blais <guillaume.blais@edu.cegepgarneau.ca>
2023-12-01 10:13:25 -0500

    gestion du fragment de liste magasin

--- app/src/main/java/com/example/tp4_progmobile/model/Item.kt
+++ app/src/main/java/com/example/tp4_progmobile/model/Item.kt
@@ -1,7 +1,5 @@
 package com.example.tp4_progmobile.model
 
-import java.util.*
-
 data class Item(
     val id: String? = null,
     val nom: String? = null,
--- app/src/main/java/com/example/tp4_progmobile/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tp4_progmobile/ui/home/HomeFragment.kt
@@ -1,20 +1,21 @@
+// HomeFragment.kt
+
 package com.example.tp4_progmobile.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tp4_progmobile.databinding.FragmentHomeBinding
+import com.example.tp4_progmobile.model.Item // Remplacez par le vrai modèle de vos données
+import com.example.tp4_progmobile.ui.ItemAdapter
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +29,19 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Créez une liste d'exemple (remplacez-la par votre liste réelle)
+        val itemList: List<Item> = listOf(
+            Item("Nom1", "Catégorie1", 1, 10.0),
+            Item("Nom2", "Catégorie2", 2, 20.0),
+            // Ajoutez d'autres éléments
+        )
+
+        val adapter = ItemAdapter(itemList)
+        recyclerView.adapter = adapter
+
         return root
     }
 
